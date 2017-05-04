4e80d6878be7775da25b763d91d5c382fe8d1f01
Nan YE <dailystudio2010@gmail.com>
2017-05-04 10:24:01 +0800

    [apiaicommon][DEV]: add common classes;

---
+++ apiaicommon/src/main/java/com/dailystudio/apiaicommon/APIAICommonDatabaseContentProvider.java
@@ -0,0 +1,10 @@
+package com.dailystudio.apiaicommon;
+
+import com.dailystudio.dataobject.database.DatabaseConnectivityProvider;
+
+/**
+ * Created by nanye on 17/5/4.
+ */
+
+public class APIAICommonDatabaseContentProvider extends DatabaseConnectivityProvider{
+}
---
+++ apiaicommon/src/main/java/com/dailystudio/apiaicommon/loader/LoaderIds.java
@@ -0,0 +1,11 @@
+package com.dailystudio.apiaicommon.loader;
+
+/**
+ * Created by nanye on 17/5/2.
+ */
+
+public class LoaderIds {
+
+    public static final int LOADER_AGENTS = 0x001;
+
+}
